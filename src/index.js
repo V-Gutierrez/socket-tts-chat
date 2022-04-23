b9edee23b8e54d8c8799425adebdaefa57778758
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-04-23 16:07:31 -0300

    fix: change cors and port settings

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const path = require('path')
 const { Server } = require('socket.io')
 
 const app = express()
-const server = app.listen(5000)
+const server = app.listen(process.env.PORT || 5000)
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5000', '192.168.0.56:5000', 'localhost:3000'],
+    origin: ['http://localhost:5000', '192.168.0.56:5000', 'localhost:3000', 'https://ttschat-vgutierrez.herokuapp.com/'],
   }
 })
 
