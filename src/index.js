4826819e0c4ad8aa76fd9f59a8146c5424664ce1
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-04-23 16:02:13 -0300

    Remove profile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
-import express from 'express'
-import path from 'path'
-import { Server, Socket } from 'socket.io';
+const express = require('express')
+const path = require('path')
+const { Server } = require('socket.io')
 
 const app = express()
 const server = app.listen(5000)
@@ -10,9 +10,9 @@ const io = new Server(server, {
   cors: {
     origin: ['http://localhost:5000', '192.168.0.56:5000', 'localhost:3000'],
   }
-});
+})
 
-io.on("connection", (socket: Socket) => {
+io.on("connection", (socket) => {
   const connections = Array.from(io.sockets.sockets).map(socket => socket[0]).length
 
   socket.removeAllListeners()
@@ -20,10 +20,10 @@ io.on("connection", (socket: Socket) => {
   socket.on("SEND_MESSAGE", ({ message, user }) => {
     socket.broadcast.emit("BROADCAST", { message, user: user })
   })
-});
+})
 
-app.use(express.static(path.resolve(__dirname, '../views/build')));
+app.use(express.static(path.resolve(__dirname, '../views/build')))
 
 app.get('*', (_, res) => {
-  res.sendFile(path.resolve(__dirname, '../views/build', 'index.html'));
-});
+  res.sendFile(path.resolve(__dirname, '../views/build', 'index.html'))
+})
